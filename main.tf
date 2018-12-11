fd4b81611ebb6ca358e899979ac213ec8075a5ce
de CLERCK <j2clerck@gmail.com>
2018-12-11 11:25:12 +0100

    first push

---
+++ main.tf
@@ -0,0 +1,46 @@
+# © 2018 Amazon Web Services, Inc. or its affiliates. All Rights Reserved. 
+# This AWS Content is provided subject to the terms of the AWS Customer Agreement 
+# available at http://aws.amazon.com/agreement or other written agreement between 
+# Customer and Amazon Web Services, Inc.
+
+module "vpc" {
+  source   = "modules/vpc"
+  vpc_name = "DEMO"
+  vpc_cidr = "192.168.0.0/24"
+}
+
+
+data "aws_ami" "amzn-linux" {
+  most_recent = true
+
+  filter {
+    name   = "product-code"
+    values = ["aw0evgkw8e5c1q413zgy5pjce"]
+  }
+
+  filter {
+    name   = "owner-alias"
+    values = ["aws-marketplace"]
+  }
+
+}
+resource "aws_security_group" "allow_all" {
+  name        = "allow_all"
+  description = "Allow all inbound traffic"
+  vpc_id      = "${module.vpc.vpc_id}"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+}
---
+++ ssh.tf
@@ -0,0 +1,32 @@
+# © 2018 Amazon Web Services, Inc. or its affiliates. All Rights Reserved. 
+# This AWS Content is provided subject to the terms of the AWS Customer Agreement 
+# available at http://aws.amazon.com/agreement or other written agreement between 
+# Customer and Amazon Web Services, Inc.
+resource "aws_instance" "jenkins_master_ssh" {
+  ami           = "${data.aws_ami.amzn-linux.id}"
+  instance_type = "i3.metal"
+  key_name      = "${aws_key_pair.generated_key.key_name}"
+  subnet_id     = "${module.vpc.public_subnets[0]}"
+  vpc_security_group_ids = ["${aws_security_group.allow_all.id}"  ]
+  iam_instance_profile = "ec2-ssm-role"
+
+  tags {
+    Name = "Ansible+SSH"
+  }
+}
+
+resource "tls_private_key" "tmp_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+resource "aws_key_pair" "generated_key" {
+  key_name_prefix   = "redeploy_temp_key"
+  public_key = "${tls_private_key.tmp_key.public_key_openssh}"
+}
+resource "local_file" "ssh_key"{
+  content = "${tls_private_key.tmp_key.private_key_pem}"
+  filename = "${path.cwd}/key.pem"
+  provisioner "local-exec" {
+    command = "chmod 600 ${path.cwd}/key.pem"
+}
+}
